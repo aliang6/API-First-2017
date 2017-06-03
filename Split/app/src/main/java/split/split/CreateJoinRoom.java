35973a5806aa9ac0a1d8fce49b734734bee41711
Andy Liang <andy.liang.1@stonybrook.edu>
2017-06-03 11:37:26 -0400

    Buttons

--- Split/app/src/main/java/split/split/CreateJoinRoom.java
+++ Split/app/src/main/java/split/split/CreateJoinRoom.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 
 public class CreateJoinRoom extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_join_room);
+
     }
 }
