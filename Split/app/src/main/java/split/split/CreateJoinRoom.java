9b108fb1c872e00964091305183d9d4b3f79e7a0
Andy Liang <andy.liang.1@stonybrook.edu>
2017-06-03 11:20:39 -0400

    Added CreateJoinRoom Page

---
+++ Split/app/src/main/java/split/split/CreateJoinRoom.java
@@ -0,0 +1,13 @@
+package split.split;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateJoinRoom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_join_room);
+    }
+}
