83226c0a0debdbef3023956f63d3109fe733394c
Andy Liang <andy.liang.1@stonybrook.edu>
2017-06-04 00:22:13 -0400

    Implemented Tapper and Started Result Page

--- Split/app/src/main/java/split/split/VotePage.java
+++ Split/app/src/main/java/split/split/VotePage.java
@@ -2,6 +2,9 @@ package split.split;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class VotePage extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class VotePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vote_page);
+
+        final Button tapper = (Button)findViewById(R.id.TapperButton);
+
+        tapper.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View view) {
+                        Intent intent = new Intent(view.getContext(), Tapper.class);
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 }
